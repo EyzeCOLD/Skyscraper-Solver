3471dacc10c07f67493996d83cd1df03c9a03e06
Tarik Pinarli <tpinarli@c2r4p2.hive.fi>
2024-07-07 20:47:48 +0300

    row_loader done

--- ex00/row_loader.c
+++ ex00/row_loader.c
@@ -6,40 +6,33 @@
 /*   By: tpinarli <tpinarli@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/07 18:39:16 by tpinarli          #+#    #+#             */
-/*   Updated: 2024/07/07 19:15:18 by tpinarli         ###   ########.fr       */
+/*   Updated: 2024/07/07 20:46:50 by tpinarli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+
 int	row_loader(char *str1, char *str2, int possible_rows[24][4])
 {
-	int	i;
-	int	j;
+	int	row;
+	int	digit;
 	int	k;
 
-	i = 0;
+	row = 0;
 	k = 0;
-	while (i < 12)
+	while (row < 24)
 	{
-		j = 0;
-		while (j < 4)
+		digit = 0;
+		while (digit < 4)
 		{
-			possible_rows[i][j] = str1[k] - '0';
-			j++;
+			if (row < 12)
+				possible_rows[row][digit] = str1[k] - '0';
+			if (row > 11)
+				possible_rows[row][digit] = str2[k - 48] - '0';
+			digit++;
 			k++;
 		}
-		i++;
-	}
-	k = 0;
-	while (i < 24)
-	{
-		j = 0;
-		while (j < 4)
-		{
-			possible_rows[i][j] = str2[k] - '0';
-			j++;
-			k++;
-		}
-		i++;
+		row++;
 	}
 	return (0);
 }
@@ -54,7 +47,7 @@ int	row_loader(char *str1, char *str2, int possible_rows[24][4])
 	int	j;
 	int	possible_rows[24][4];
 
-	first = "432134214231243132412341431234124132143231421342";
+	first =  "432134214231243132412341431234124132143231421342";
 	second = "421324134123142321431243321423143124132421341234";
 	row_loader(first, second, possible_rows);
 	i = 0;
